d62caf1574bb4c3dccdea977e921320b465806e1
jtakamine <justin.takamine@gmail.com>
2014-12-26 18:35:09 -0800

    make test non-trivial

--- dodecci/dodecci_test.go
+++ dodecci/dodecci_test.go
@@ -1,16 +1,62 @@
 package main
 
 import (
+	"os"
 	"os/exec"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
-	cmd := exec.Command("fig", "up")
-	err := cmd.Run()
+	var err error
+	var cmd *exec.Cmd
 
+	cmd = exec.Command("fig", "kill")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Dir = ".."
+
+	err = cmd.Run()
 	if err != nil {
 		t.Error(err)
 	}
 
+	cmd = exec.Command("fig", "rm", "--force")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Dir = ".."
+
+	err = cmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	cmd = exec.Command("fig", "build")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Dir = ".."
+
+	err = cmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	cmd = exec.Command("fig", "up", "-d")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Dir = ".."
+
+	err = cmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
+
+	payload := "{\"repository\":{\"id\":1234567,\"ssh_url\":\"git@github.com:jtakamine/dodecahedronci.git\", \"clone_url\":\"https://github.com/progrium/logspout.git\"}}"
+	cmd = exec.Command("curl", "-H", "Content-Type: application/json", "-d", payload, "http://localhost:8000")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	err = cmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
 }
