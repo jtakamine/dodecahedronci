1e606eff5756a20d3c8bb0e1c8e499d8e7b51603
jtakamine <justin.takamine@gmail.com>
2014-12-27 18:51:30 -0800

    add dodecregistry and dodecdeploy to fig.yml; supporting changes

--- dodecregistry/http.go
+++ dodecregistry/http.go
@@ -1,3 +1,20 @@
 package main
 
-import ()
+import (
+	"net/http"
+)
+
+func ListenAndServe(addr string) (err error) {
+	http.HandleFunc("/", httpHandle)
+
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func httpHandle(w http.ResponseWriter, r *http.Request) {
+
+}
